b63e10987bad00a3d0cb717ec39f8a6d19ee9c7d
Kalmi Dilara <kalmid.98@gmail.com>
2026-03-30 10:05:41 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// connect DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(4000, () => console.log("Server running on port 4000"));
+  })
+  .catch(err => console.log(err));
